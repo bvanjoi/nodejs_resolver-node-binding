3df70d809f41de9ebe9b3fda22b39d890ef03f65
ByteDance <zhangbohan.0@bytedance.com>
2022-06-07 15:52:36 +0800

    feat: optimized args

--- __test__/index.spec.ts
+++ __test__/index.spec.ts
@@ -2,11 +2,37 @@ import path from 'path'
 
 import test from 'ava'
 
-import factory from '../index'
+import factory, { RawResolverOptions } from '../index'
 
 test('sync function from native code', (t) => {
-  const resolver = factory.create(JSON.stringify({}))
-  const result = factory.resolve(resolver, __dirname, './lib.js')
-  t.is(result.path, path.resolve(__dirname, './lib.js'))
+  const resolver = factory.create({})
+  const result = factory.resolve(resolver, __dirname, './fixture/lib')
+  t.is(result.path, path.resolve(__dirname, './fixture/lib.js'))
+  t.is(result.status, true)
+})
+
+test('resolve do not exist file', (t) => {
+  const resolver = factory.create({})
+  let count = 0
+  let encounterError = false
+  try {
+    count += 1
+    factory.resolve(resolver, __dirname, './lib')
+    count += 1
+  } catch (err: any) {
+    encounterError = true
+    t.assert((err.message as string).includes('Resolve ./lib failed in'))
+  }
+  t.is(count, 1)
+  t.is(encounterError, true)
+})
+
+test('extensions options', (t) => {
+  const resolverOptions: RawResolverOptions = {
+    extensions: ['ts', 'js'],
+  }
+  const resolver = factory.create(resolverOptions)
+  const result = factory.resolve(resolver, __dirname, './fixture/lib')
+  t.is(result.path, path.resolve(__dirname, './fixture/lib.ts'))
   t.is(result.status, true)
 })
